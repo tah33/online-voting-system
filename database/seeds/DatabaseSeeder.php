3659fa71a9dae7406157d5ae733dc145aea086db
Tanvir Ahmed Hera <tahmedhera@gmail.com>
2019-10-25 15:23:46 +0600

    "Database was created and seeding is for admin created"

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,19 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\User;
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        User::create([
+            'name' =>'Saria',
+            'username' =>'saria',
+            'email' =>'saria@gmail.com',
+            'password' =>bcrypt('123456'),
+            'role' =>'admin',
+        ]);
     }
 }
