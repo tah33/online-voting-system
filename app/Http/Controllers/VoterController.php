27f2e13ff952dfa4e65a302638e395b2b3cf47af
Tanvir Ahmed <Tanvir Ahmed>
2019-12-10 21:58:52 +0600

    Voter section table is fixed

--- app/Http/Controllers/VoterController.php
+++ app/Http/Controllers/VoterController.php
@@ -12,11 +12,7 @@ use Illuminate\Http\Request;
 
 class VoterController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
      public function index()
     {
         $ids = [];
@@ -31,33 +27,17 @@ class VoterController extends Controller
     }
    
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Voter  $voter
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $candidate = Candidate::find($id);
@@ -88,36 +68,17 @@ class VoterController extends Controller
         return back()->with('success','Your valuable vote was successfully submitted');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Voter  $voter
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $election = Election::find($id);
         return view('voter.election',compact('election'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Voter  $voter
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Voter $voter)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Voter  $voter
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Voter $voter)
     {
         //
--- resources/views/voter/index.blade.php
+++ resources/views/voter/index.blade.php
@@ -1,62 +1,68 @@
 @extends('layouts.app')
 @section('content')
 @if ($message = Session::get('warning'))
-        <div class="alert alert-danger alert-block">
-            <button type="button" class="close" data-dismiss="alert">×</button>
-            <strong>{{ $message }}</strong>
-        </div>
-    @endif
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success alert-block">
-            <button type="button" class="close" data-dismiss="alert">×</button>
-            <strong>{{ $message }}</strong>
-        </div>
-    @endif
-    <div class="row"> 
-        <div class="box">
-            <div class="box-body">
-                <table class="table table-hover table-bordered">
-                    <caption>Voting Area</caption>
-                    <thead>
+<div class="alert alert-danger alert-block">
+    <button type="button" class="close" data-dismiss="alert">×</button>
+    <strong>{{ $message }}</strong>
+</div>
+@endif
+@if ($message = Session::get('success'))
+<div class="alert alert-success alert-block">
+    <button type="button" class="close" data-dismiss="alert">×</button>
+    <strong>{{ $message }}</strong>
+</div>
+@endif
+<div class="row">
+    <div class="box">
+        <div class="box-body">
+            <table class="table table-hover table-bordered">
+                <caption>Voting Area</caption>
+                <thead>
                     <tr>
                         <th style="text-align: center">Election Name</th>
                         <th style="text-align: center">Candidates</th>
                         <th style="text-align: center">Action</th>
                     </tr>
-                    </thead>
-                    <tbody align="center">
+                </thead>
+                <tbody align="center">
                     @foreach ($elections as $key => $election)
-                        <tr>
-                            <td style="text-align: center">{{ $election->name }}</td>
-                            <td style="text-align: center">
-                              @if($candidates)
-                              @foreach($candidates as $key => $candidate)
-                              <table class="table table-hover table-bordered">
-                              <tr><td>
-                                <b>{{$key+1}} </b>:  <b>{{$candidate->user['name']}}</b> 
-                                </td></tr>
+                    <tr>
+                        <td style="text-align: center">{{ $election->name }}</td>
+                        <td style="text-align: center">
+                            @if($candidates)
+                            @foreach($election->candidates as $key => $candidate)
+                            <table class="table table-hover table-bordered">
+                                <tr>
+                                    <td>
+                                        <b>{{$key+1}} </b>: <b>{{$candidate->user['name']}}</b>
+                                    </td>
+                                </tr>
                             </table>
-                              @endforeach
-                              @endif
-                            </td>
-                            <td>
-                                @if($candidates)
-                              @foreach($candidates as $key => $candidate)
-                              @if($candidate->user['area'] == Auth::user()->area || Auth::user()->role == 'admin')
-                              <table class="table table-hover table-bordered">
-                                <tr><td style=" text-align: center ">
-                                <a href="{{url('voters', $candidate->id)}}" class="btn btn-success btn-sm" style="margin-top: -10px" onclick="return confirm('Are you Sure You want to vote this candidates?')"><i class="glyphicon glyphicon-ok"></i></a> </td>
+                            @endforeach
+                            @endif
+                        </td>
+                        <td>
+                            @if($candidates)
+                            @foreach($election->candidates as $key => $candidate)
+                            @if($candidate->user['area'] == Auth::user()->area || Auth::user()->role == 'admin')
+                            <table class="table table-hover table-bordered">
+                                <tr>
+                                    <td style=" text-align: center ">
+                                        <a href="{{url('voters', $candidate->id)}}" class="btn btn-success btn-sm"
+                                            style="margin-top: -10px"
+                                            onclick="return confirm('Are you Sure You want to vote this candidates?')"><i
+                                                class="glyphicon glyphicon-ok"></i></a> </td>
                                 </tr>
                             </table>
                             @endif
                             @endforeach
                             @endif
-                            </td>
-                                </tr>
+                        </td>
+                    </tr>
                     @endforeach
-                    </tbody>
-                </table>
-            </div>
+                </tbody>
+            </table>
         </div>
     </div>
+</div>
 @endsection
