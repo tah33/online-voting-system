4f4a6ddd6cdf4dc9ad95e2ad6f565596d84c9eb0
Tanvir Ahmed <Tanvir Ahmed>
2019-12-10 22:57:14 +0600

    result is calculated successfully

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -12,44 +12,23 @@ use Carbon\Carbon;
 use DB;
 class ResultController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         $elections = Election::where('status',1)->get();
         return view('results.index',compact('elections'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $users = '';
@@ -63,12 +42,7 @@ class ResultController extends Controller
 
         return view('results.show',compact('users'));
     }
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit($id)
     {
         $users = User::where('area',$id)->where('role','candidate')->get();
@@ -78,13 +52,6 @@ class ResultController extends Controller
         return view('results.winner',compact('users','max','users_max_vote','voters'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function result($id)
     {
         $seat = Party::where('election_id',$id)->orderBy('id','desc')
@@ -93,32 +60,34 @@ class ResultController extends Controller
         $winner = Party::where('election_id',$id)->orderBy('id','desc')
                 ->where('seats',$seat)->first();
         $candidates =Candidate::where('election_id',$id)->groupBy('area')->get();
+        if($winner->seat == 0){
         foreach ($candidates as $key => $candidate) {
-            $areas [] = $candidate->area;
-            $max_vote = Candidate::where('election_id',$id)
-                        ->where('area',$candidate->area)->max('votes');
+            // $areas [] = $candidate->area;
+             $max_vote = Candidate::where('election_id',$id)
+                         ->where('area',$candidate->area)->max('votes');
 
+        $areaWinner = Candidate::where('votes',$max_vote)
+                        ->where('election_id',$id)
+                        ->where('area',$candidate->area)->first();
         $party = Party::where('user_id',$candidate->user_id)
-                        ->where('election_id',$id)->latest()->first();
-        $party->seats = $party->seats + 1;
-        $party->save();
-        $parties = Party::where('user_id','!=',$candidate->user_id)
+                ->where('election_id',$candidate->election_id)->latest()->first();
+        if(!empty($party)){
+            $count = 1;
+            $party->seats =  $count;
+            $party->save();
+            $count++;
+            }
+        $looserparties = Party::where('user_id','!=',$candidate->user_id)
                         ->where('election_id',$id)->orderBy('id','desc')->get();
-                        foreach ($parties as $key => $party) {
+                        foreach ($looserparties as $key => $party) {
                             $party->seats += 0;
                             $party->save();
                         }
         }
-
+    }
         return view('results.result',compact('parties','seat','winner'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function winner()
     {
         $elections = Election::where('status',1)->get();
